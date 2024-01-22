451670f6d9c9e06303fb6b19ab8cd22a78e8125a
Pedro Henrique <pedro.semear@gmail.com>
2024-01-22 18:25:11 -0300

    setup project

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,12 @@
+export const defaultTheme = {
+    white: '#fff',
+    brown: '#4F372F',
+    'inactive':'#DADADA',
+    'background': '#FFFFFF4D',
+
+    'main':'#121212',
+    'secondary': '#464646',
+    'gray':'#8A94A4',
+
+
+  } as const
