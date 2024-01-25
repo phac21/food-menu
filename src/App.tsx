bfbc80ef3abe48eee741e53a736ca4073247413a
Pedro Henrique <pedro.semear@gmail.com>
2024-01-24 21:21:56 -0300

    add/ Menu screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import Menu from "./pages/Menu";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle/>
-      <h1>Helo world</h1>
+      <Menu/>
     </ThemeProvider>
   )
 }
