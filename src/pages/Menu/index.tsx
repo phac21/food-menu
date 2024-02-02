338f4e217c0bbc31a43edda3308f65514caab062
Pedro Henrique <pedro.semear@gmail.com>
2024-02-02 00:17:52 -0300

    add/ Banner on Menu screen

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import MenuHamburguer from '../../components/MenuHamburguer';
+import { Container } from './styles';
 
 const Menu: React.FC = () => {
   return (
-  <div>
+  <Container>
     <MenuHamburguer/>
-  </div>
+    <img src="https://preodemo.gumlet.io/usr/venue/7602/web/646fbf3abf9d0.png" alt="" />
+  </Container>
   )
 }
 
