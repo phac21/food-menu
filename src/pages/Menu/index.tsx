b8ed8ec6232807ea48c3c52eaac0adc9abe2730f
Pedro Henrique <pedro.semear@gmail.com>
2024-02-02 00:21:34 -0300

    fix/ banner img

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import MenuHamburguer from '../../components/MenuHamburguer';
 import { Container } from './styles';
-import banner from'../../assets/banner.svg';
 
 const Menu: React.FC = () => {
   return (
   <Container>
     <MenuHamburguer/>
-    <img src={banner} alt="banner contendo hamburguer" />
+    <img src="https://preodemo.gumlet.io/usr/venue/7602/web/646fbf3abf9d0.png"  alt="banner com hamburguer" />
+
   </Container>
   )
 }
